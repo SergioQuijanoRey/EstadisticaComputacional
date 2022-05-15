e505ee206240938eebd431ecac5a492427ed57c0
Sergio <sergiquijano@gmail.com>
2022-05-15 19:43:44 +0200

    feat: trabajo algo la practica 8, en formato .Rmd

--- SesionesDiarias/Sesion08--05-05-2022.r
+++ SesionesDiarias/Sesion08--05-05-2022.r
@@ -1,16 +1,16 @@
-# Autor: Sergio Quijano Rey
-# Fecha: 05.05.2022
-#
-# NOTA: como estamos mostrando mensajes por pantalla, lo recomendable es ejecutar o bien el script
-# por lineas en un REPL o bien ejecutar todo el script usando `R --no-echo -f Sesion08--05-05-2022.r`
-# para evitar que se muestren por pantalla tambien las lineas de codigo, haciendo que los mensajes
-# por pantalla sean ilegibles
+#' Autor: Sergio Quijano Rey
+#' Fecha: 05.05.2022
+#'
+#' NOTA: como estamos mostrando mensajes por pantalla, lo recomendable es ejecutar o bien el script
+#' por lineas en un REPL o bien ejecutar todo el script usando `R --no-echo -f Sesion08--05-05-2022.r`
+#' para evitar que se muestren por pantalla tambien las lineas de codigo, haciendo que los mensajes
+#' por pantalla sean ilegibles
 
-# Cargamos los datos
+#' Cargamos los datos
 message("Cargando los datos")
 message("")
 
-# Hacemos as.is = NA para que todos los factores se identifiquen en el dataframe como tal
+#' Hacemos as.is = NA para que todos los factores se identifiquen en el dataframe como tal
 hatco <- read.csv("hatco2.csv", header = TRUE, as.is = NA)
 message("Las primeras filas de los datos leidos son:")
 print(head(hatco, 10))
@@ -20,8 +20,8 @@ message("La estructura de los datos leidos es:")
 print(str(hatco))
 message("")
 
-# Representamos graficamente las variables con las que vamos a estar trabajando
-# Nos interesa ver las correlaciones entre las variables
+#'' Representamos graficamente las variables con las que vamos a estar trabajando
+#' Nos interesa ver las correlaciones entre las variables
 message("Mostrando graficamente las variables con las que trabajamos")
 plot(hatco[,c(6:13)])
 message("")
@@ -38,18 +38,18 @@ message("   - Servconj y velocidad")
 message("   - Servconj y precio (muy ligeramente)")
 message("")
 
-# Realizamos el ajuste lineal
+#' Realizamos el ajuste lineal
 mod1 <- lm(fidelidad~velocidad+precio+flexprec+imgfabri+servconj+imgfvent+calidadp, hatco)
 message("El ajuste lineal realizado es:")
 print(mod1)
 message("")
 
-# Miramos la bondad del ajuste
+#' Miramos la bondad del ajuste
 message("Bondad del ajuste realizado:")
 print(summary(mod1))
 message("")
 
-# Realizamos el test ANOVA
+#' Realizamos el test ANOVA
 anovares <- anova(mod1)
 message("El resultado de hacer el test ANOVA es:")
 print(anovares)
@@ -66,11 +66,11 @@ message("A un nivel de significación del 1%")
 message("- Intercept (\\beta_0) se rechaza pues su p-valor es 0.044 > 0.01")
 message("")
 
-# Estudiamos la homocedasticidad
+#' Estudiamos la homocedasticidad
 message("ESTUDIAMOS LA HOMOCEDASTICIDAD")
 message("==============================")
 residuos <- rstandard(mod1)
-predicciones <- mod1$fitted.values # Las predicciones ya nos las da el objeto devuelto por lm
+predicciones <- mod1$fitted.values #' Las predicciones ya nos las da el objeto devuelto por lm
 message("")
 
 message("Grafico de dispersion de los pares \\hat{y_i}, r_i:")
@@ -86,6 +86,6 @@ message("-> Ademas, la dispersion parece uniforme, salvo en valores de la variab
 message("")
 
 
-# Estudiamos la incorrelacion
+#' Estudiamos la incorrelacion
 message("ESTUDIAMOS LA INCORRELACION")
 message("==============================")
